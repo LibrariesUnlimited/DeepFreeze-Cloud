7ed33b06bb2b972a4f0e2138df03e0929445dd4f
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2023-06-08 16:33:06 +0100

    additions and updates

--- POC - autologon.ps1
+++ POC - autologon.ps1
@@ -4,4 +4,4 @@ $registryLocation = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon
 $null = New-ItemProperty -Name "DefaultUserName" -Path $registryLocation -PropertyType string -Value "LUTestUser" -Force -EA Stop
 $null = New-ItemProperty -Name "AutoAdminLogon" -Path $registryLocation -PropertyType string -Value "1" -Force -EA Stop
 $null = New-ItemProperty -Name "DefaultPassword" -Path $registryLocation -PropertyType string -Value "FaronicsTest45!" -Force -EA Stop
-
+$null = New-ItemProperty -Name "ForceAutoLogon" -Path $registryLocation -PropertyType string -Value "1" -Force -EA Stop
