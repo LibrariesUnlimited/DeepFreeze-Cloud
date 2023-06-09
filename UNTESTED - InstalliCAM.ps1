2f66be18a0cee798f8a03af441c5c85d00d1af85
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2023-06-09 09:46:41 +0100

    test version 2

--- UNTESTED - InstalliCAM.ps1
+++ UNTESTED - InstalliCAM.ps1
@@ -1,22 +1,19 @@
 # Script to download and install iCAM Workstation Client and Print Client in order
 # Not installing UltraViewer as it is installed from DeepFreeze so not needed
 
-#still test download location
-
-if(-not(Test-Path "C:\InstallTemp\")) {
-	New-Item "C:\InstallTemp\" -ItemType Directory -Force
+if(-not(Test-Path "C:\Program Files (x86)\iCAM\")) {
+	New-Item "C:\Program Files (x86)\iCAM\" -ItemType Directory -Force
 }
 
-Invoke-WebRequest "https://devon.imil.uk/adverts/test/iCAM Workstation Control Client 5.9.1.msi" -OutFile "C:\InstallTemp\iCAM Workstation Control Client 5.9.1.msi"
+Invoke-WebRequest "https://devon.imil.uk/adverts/test/iCAM Workstation Control Client 5.9.1.msi" -OutFile "C:\Program Files (x86)\iCAM\iCAM Workstation Control Client 5.9.1.msi"
 
-Invoke-WebRequest "https://devon.imil.uk/adverts/test/iCAM Print Client 4.7.0.1000.msi" -OutFile "C:\InstallTemp\iCAM Print Client 4.7.0.1000.msi"
+Invoke-WebRequest "https://devon.imil.uk/adverts/test/iCAM Print Client 4.7.0.1000.msi" -OutFile "C:\Program Files (x86)\iCAM\iCAM Print Client 4.7.0.1000.msi"
 
 # Start-Process -Wait means the MSIExec.exe task will be required to finished before moving on to the next line of the Powershell Script
-#need to test quotes work
-Start-Process msiexec.exe -Wait -ArgumentList '/i "C:\InstallTemp\iCAM Workstation Control Client 5.9.1.msi" ADDLOCAL=iCAMWorkstationControlClient,Services,iCAMSCR,KeyboardFilter /qn /norestart'
+Start-Process msiexec.exe -Wait -ArgumentList '/i "C:\Program Files (x86)\iCAM\iCAM Workstation Control Client 5.9.1.msi" ADDLOCAL=iCAMWorkstationControlClient,Services,iCAMSCR,KeyboardFilter /qn /norestart'
 
-#need to test driver installs correctly as well
-Start-Process msiexec.exe -Wait -ArgumentList '/i "C:\InstallTemp\iCAM Print Client 4.7.0.1000.msi" /qn /norestart'
 
-# Clean up downloaded files
-Remove-Item -Path "C:\InstallTemp" -Recurse -Force
+Start-Process msiexec.exe -Wait -ArgumentList '/i "C:\Program Files (x86)\iCAM\iCAM Print Client 4.7.0.1000.msi" /qn /norestart'
+
+#Notes : Workstation Control Client did not show up in Add/Remove Programs after first test, trying leaving the install files on the computer this time to see if it works.
+#Installer did work however
