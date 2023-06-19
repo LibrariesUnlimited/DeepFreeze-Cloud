f952c30dc9fc43cd0c35a6c3b28a0378ae130850
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2023-06-19 14:35:21 +0100

    adding mailto

--- UNTESTED - WindowsRegistrySettings.ps1
+++ UNTESTED - WindowsRegistrySettings.ps1
@@ -6,5 +6,17 @@ Set-ItemProperty -Path "HKLM\SOFTWARE\Microsoft\PolicyManager\default\Start\Hide
 
 # Set Mailto to use bat file and go to website instead of opening Outlook
 # Currently will not work as Outlook not installed but also need to get launchurl.bat file from current public PCs and decide where to put them.
-# Could this be one script which sets the correct URL based on computername?
-# Set-ItemProperty -Path "HKLM\SOFTWARE\Classes\Outlook.URL.mailto.15\shell\open\command" -Value "C:\Windows\System32\cmd.exe /c C:\xxxxxxxxx\launchurl.bat https://www.devonlibraries.org.uk/web/arena/webmaillinks %1"
+
+$computerPrefix = $env:COMPUTERNAME.Substring(0,3)
+<#
+switch ($computerPrefix) {
+    {($_ -eq "BRI") -or ($_ -eq "CHU") -or ($_ -eq "PAI") -or ($_ -eq "TQY")}
+        {
+            Set-ItemProperty -Path "HKLM\SOFTWARE\Classes\Outlook.URL.mailto.15\shell\open\command" -Value "C:\Windows\System32\cmd.exe /c C:\xxxxxxxxx\launchurl.bat https://www.torbaylibraries.org.uk/web/arena/webmaillinks %1"
+        }
+    Default
+        {
+            Set-ItemProperty -Path "HKLM\SOFTWARE\Classes\Outlook.URL.mailto.15\shell\open\command" -Value "C:\Windows\System32\cmd.exe /c C:\xxxxxxxxx\launchurl.bat https://www.devonlibraries.org.uk/web/arena/webmaillinks %1"
+        }
+}
+#>
