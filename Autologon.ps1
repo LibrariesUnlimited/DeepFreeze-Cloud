8a747e8d2379f8ee0db012fdba27018efbaa0f42
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2023-06-27 16:01:21 +0100

    adding debugging

--- Autologon.ps1
+++ Autologon.ps1
@@ -1,7 +1,25 @@
 #Imaging script to set Public User account to automatically log on after maintenance.
 
 $registryLocation = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+
+#debugging
+$debugLog = "C:\Program Files\Libraries Unlimited\autologondebug.log"
+
+Write-Output "Starting script at $(Get-Date -Format "dd-MM-yyyy HH:mm:ss")..." | Out-File -FilePath $debugLog -Append
+Get-ItemProperty -Path $registryLocation | Out-File -FilePath $debugLog -Append
+
+Write-Output "Changing Registry Settings" | Out-File -FilePath $debugLog -Append
+Write-Output "---------------------------------------------------------" | Out-File -FilePath $debugLog -Append
+
 Set-ItemProperty -Path $registryLocation -Name "DefaultUserName" -Value "LUTestUser"
 Set-ItemProperty -Path $registryLocation -Name "AutoAdminLogon" -Value "1" -Type String
 Set-ItemProperty -Path $registryLocation -Name "DefaultPassword" -Value "FaronicsTest45"
 Set-ItemProperty -Path $registryLocation -Name "ForceAutoLogon" -Value "1" -Type String
+
+
+Write-Output "Registry Settings Changed" | Out-File -FilePath $debugLog -Append
+Write-Output "---------------------------------------------------------" | Out-File -FilePath $debugLog -Append
+
+Get-ItemProperty -Path $registryLocation | Out-File -FilePath $debugLog -Append
+Write-Output "Ending script at $(Get-Date -Format "dd-MM-yyyy HH:mm:ss")..." | Out-File -FilePath $debugLog -Append
+Write-Output "---------------------------------------------------------" | Out-File -FilePath $debugLog -Append
