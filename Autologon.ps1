34c5786e09c700c14723031e7390f4b288942de1
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2023-06-28 15:20:16 +0100

    different logging

--- Autologon.ps1
+++ Autologon.ps1
@@ -1,5 +1,7 @@
 #Imaging script to set Public User account to automatically log on after maintenance.
+Start-Transcript "C:\Program Files\Libraries Unlimited\autologontranscript.txt"
 
+& "C:\Windows\SysWOW64\DFC.exe" get /isfrozen
 $registryLocation = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 
 #debugging
@@ -11,6 +13,7 @@ Get-ItemProperty -Path $registryLocation | Out-File -FilePath $debugLog -Append
 Write-Output "Changing Registry Settings" | Out-File -FilePath $debugLog -Append
 Write-Output "---------------------------------------------------------" | Out-File -FilePath $debugLog -Append
 
+Set-ItemProperty -Path $registryLocation -Name "LastUsedUsername" -Value "LUTestUser"
 Set-ItemProperty -Path $registryLocation -Name "DefaultUserName" -Value "LUTestUser"
 Set-ItemProperty -Path $registryLocation -Name "AutoAdminLogon" -Value "1" -Type String
 Set-ItemProperty -Path $registryLocation -Name "DefaultPassword" -Value "FaronicsTest45!"
