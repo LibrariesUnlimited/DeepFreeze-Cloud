78e41c4728db21103436ab91c4b1f7894abc910b
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2023-06-28 10:23:30 +0100

    certificates not working

--- BuildScript.ps1
+++ BuildScript.ps1
@@ -529,9 +529,9 @@ $params = @{
 }
 Import-Certificate @params
 
-# Clean up temp files
-Remove-Item -Path "C:\Windows\Temp\Fortinet_CA_SSL(1).cer" -Force
-Remove-Item -Path "C:\Windows\Temp\Fortinet_CA_SSL(2).cer" -Force
+# Clean up temp files (removed temporarily as it isn't working)
+#Remove-Item -Path "C:\Windows\Temp\Fortinet_CA_SSL(1).cer" -Force
+#Remove-Item -Path "C:\Windows\Temp\Fortinet_CA_SSL(2).cer" -Force
 #endregion ImportCertificates
 
 #region CreateStartupScheduledTask
--- UNTESTED - ImportFilterCertificates.ps1
+++ UNTESTED - ImportFilterCertificates.ps1
@@ -2,13 +2,20 @@
 
 #Import Fortiguard Certificates into Root Certificate Store
 
-#Download file to local machine
 Invoke-WebRequest "https://devon.imil.uk/adverts/test/Fortinet_CA_SSL(1).cer" -OutFile "C:\Windows\Temp\Fortinet_CA_SSL(1).cer"
+Invoke-WebRequest "https://devon.imil.uk/adverts/test/Fortinet_CA_SSL(2).cer" -OutFile "C:\Windows\Temp\Fortinet_CA_SSL(2).cer"
 
-#Import Certificate
+#Import Certificate(1)
 $params = @{
     FilePath = 'C:\Windows\Temp\Fortinet_CA_SSL(1).cer'
     CertStoreLocations = 'Cert:\LocalMachine\Root'
 }
-
 Import-Certificate @params
+
+#Import Certificate(2)
+$params = @{
+    FilePath = 'C:\Windows\Temp\Fortinet_CA_SSL(2).cer'
+    CertStoreLocations = 'Cert:\LocalMachine\Root'
+}
+Import-Certificate @params
+
