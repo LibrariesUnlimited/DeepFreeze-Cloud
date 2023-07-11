8dbe8bd558b14a3ca6627ccded49310148eb6d0c
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2023-07-11 14:03:46 +0100

    autologon test

--- BuildScript.ps1
+++ BuildScript.ps1
@@ -27,7 +27,7 @@ $registryLocation = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon
 Set-ItemProperty -Path $registryLocation -Name "DefaultUserName" -Value "LUTestUser"
 Set-ItemProperty -Path $registryLocation -Name "AutoAdminLogon" -Value "1" -Type String
 Set-ItemProperty -Path $registryLocation -Name "DefaultPassword" -Value "FaronicsTest45!"
-Set-ItemProperty -Path $registryLocation -Name "ForceAutoLogon" -Value "1" -Type String
+#Set-ItemProperty -Path $registryLocation -Name "ForceAutoLogon" -Value "1" -Type String
 #endregion AutoLogin
 
 #region ActivateOffice
