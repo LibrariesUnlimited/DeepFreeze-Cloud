0dda8d805c4c9708d8fd4caeca9c27871a1d72ff
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2023-08-24 13:54:36 +0100

    turn off audio

---
+++ WIP - Audio.ps1
@@ -0,0 +1,11 @@
+Start-Transcript -Path "C:\Program Files\Libraries Unlimited\AudioTranscript.txt"
+Write-Output "Transcript Started"
+
+$registryLocation = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\MMDevices\Audio\Render\"
+$audio = Get-ChildItem -Path $registryLocation | ForEach-Object {Get-ItemProperty -Path $_.PsPath | Where-Object {$_.DeviceState -eq 1} | Select-Object PSChildName }
+
+$derivedRegistryLocation = $registryLocation + $audio.PSChildName
+
+Set-ItemProperty -Path $derivedRegistryLocation -Name "DeviceState" -Value 268435457 -Type DWord
+
+Stop-Transcript
