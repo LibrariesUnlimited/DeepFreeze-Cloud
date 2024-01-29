99a0e6b6e6d79080a45c9c53b86eb2e92f0779c5
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2024-01-29 16:11:00 +0000

    switch code

---
+++ SWITCH - techsupport.ps1
@@ -0,0 +1,24 @@
+# Script to get Switch techsupport.txt files
+
+$content = Invoke-WebRequest -Uri "https://10.0.1.5/techsupport.txt" -SkipCertificateCheck
+
+# trying to get system log from content
+$tempArray = $content.Content.Split("- System Log -")
+
+# currently ends with </pre>
+#$tempArray[1].Trim()
+
+# messy but clears </pre> and all white space at front and end
+# (($array[1].Trim()).TrimEnd("</pre>")).Trim()
+
+# splits each line into an object with three headers, ID, Date and Status (`t is TAB)
+$objects = (($array[1].Trim()).TrimEnd("</pre>")).Trim() | ConvertFrom-Csv -Delimiter "`t" -Header "ID", "Date", "Status"
+$sObjects = $objects | Sort-Object -Property ID
+
+# The System restared abnormally
+$abnormal = $sObjects | Where-Object {$_.Status -eq "The System restared abnormally"} | Select ID
+
+#326 restarted abnormally so looking for time from ID 327
+ForEach ($a in $abnormal) {
+    $sObjects[([int]$a.ID) + 1]
+}
