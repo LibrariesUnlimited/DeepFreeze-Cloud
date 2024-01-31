a282c8c1887c0d2be6100f670bad9e01b4306564
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2024-01-31 16:45:50 +0000

    powercfg

---
+++ PowerCfg.ps1
@@ -0,0 +1,9 @@
+# Set Power Settings for default power profile
+powercfg.exe /X monitor-timeout-ac 0
+powercfg.exe /X monitor-timeout-dc 0
+powercfg.exe /X disk-timeout-ac 0
+powercfg.exe /X disk-timeout-dc 0
+powercfg.exe /X standby-timeout-ac 0
+powercfg.exe /X standby-timeout-dc 0
+powercfg.exe /X hibernate-timeout-ac 0
+powercfg.exe /X hibernate-timeout-dc 0
