00cc8f57159443e250f8dbf6f524399b8c72d3fc
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2024-02-05 14:20:10 +0000

    correcting audio csharp

--- AudioScript.ps1
+++ AudioScript.ps1
@@ -15,6 +15,46 @@ public enum ERole : uint
 [Guid("F8679F50-850A-41CF-9C72-430F290290C8"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
 internal interface IPolicyConfig
 {
+    // HRESULT GetMixFormat(PCWSTR, WAVEFORMATEX **);
+    [PreserveSig]
+    int GetMixFormat();
+    
+    // HRESULT STDMETHODCALLTYPE GetDeviceFormat(PCWSTR, INT, WAVEFORMATEX **);
+    [PreserveSig]
+	int GetDeviceFormat();
+	
+    // HRESULT STDMETHODCALLTYPE ResetDeviceFormat(PCWSTR);
+    [PreserveSig]
+    int ResetDeviceFormat();
+    
+    // HRESULT STDMETHODCALLTYPE SetDeviceFormat(PCWSTR, WAVEFORMATEX *, WAVEFORMATEX *);
+    [PreserveSig]
+    int SetDeviceFormat();
+	
+    // HRESULT STDMETHODCALLTYPE GetProcessingPeriod(PCWSTR, INT, PINT64, PINT64);
+    [PreserveSig]
+    int GetProcessingPeriod();
+	
+    // HRESULT STDMETHODCALLTYPE SetProcessingPeriod(PCWSTR, PINT64);
+    [PreserveSig]
+    int SetProcessingPeriod();
+	
+    // HRESULT STDMETHODCALLTYPE GetShareMode(PCWSTR, struct DeviceShareMode *);
+    [PreserveSig]
+    int GetShareMode();
+	
+    // HRESULT STDMETHODCALLTYPE SetShareMode(PCWSTR, struct DeviceShareMode *);
+    [PreserveSig]
+    int SetShareMode();
+	 
+    // HRESULT STDMETHODCALLTYPE GetPropertyValue(PCWSTR, const PROPERTYKEY &, PROPVARIANT *);
+    [PreserveSig]
+    int GetPropertyValue();
+	
+    // HRESULT STDMETHODCALLTYPE SetPropertyValue(PCWSTR, const PROPERTYKEY &, PROPVARIANT *);
+    [PreserveSig]
+    int SetPropertyValue();
+	
     // HRESULT STDMETHODCALLTYPE SetDefaultEndpoint(__in PCWSTR wszDeviceId, __in ERole role);
     [PreserveSig]
     int SetDefaultEndpoint(
