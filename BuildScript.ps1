3044da2c91c062ecfc6de114e25250ead530521d
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2024-02-05 12:39:15 +0000

    audio disable in startup script

--- BuildScript.ps1
+++ BuildScript.ps1
@@ -538,6 +538,123 @@ Remove-Item -Path "C:\Windows\Temp\Fortinet_CA_SSL(2).cer" -Force
 # This region creates a logon script ($script variable is content of powershell script) which is set as a Logon Scheduled Task for the publicuser account (which can be done when the user doesn't exist yet)
 # Order of tasks is oddly important as when Frozen the task bar settings revert to default about half way through the script so those settings put last.
 $script = @'
+#create C# Code
+$cSharpSourceCode = @"
+using System;
+using System.Runtime.InteropServices;
+
+public enum ERole : uint
+{
+    eConsole         = 0,
+    eMultimedia      = 1,
+    eCommunications  = 2,
+    ERole_enum_count = 3
+}
+
+[Guid("F8679F50-850A-41CF-9C72-430F290290C8"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+internal interface IPolicyConfig
+{
+    // HRESULT STDMETHODCALLTYPE SetDefaultEndpoint(__in PCWSTR wszDeviceId, __in ERole role);
+    [PreserveSig]
+    int SetDefaultEndpoint(
+        [In] [MarshalAs(UnmanagedType.LPWStr)] string wszDeviceId, 
+        [In] [MarshalAs(UnmanagedType.U4)] ERole role);
+	
+    // HRESULT STDMETHODCALLTYPE SetEndpointVisibility(PCWSTR, INT);
+    [PreserveSig]
+	int SetEndpointVisibility(
+        [In] [MarshalAs(UnmanagedType.LPWStr)] string wszDeviceId, 
+        [In] [MarshalAs(UnmanagedType.Bool)] bool bVisible);
+}
+
+[ComImport, Guid("870AF99C-171D-4F9E-AF0D-E63DF40C2BC9")]
+internal class _CPolicyConfigClient
+{
+}
+
+public class PolicyConfigClient
+{
+    public static int SetDefaultDevice(string deviceID)
+    {
+        IPolicyConfig _policyConfigClient = (new _CPolicyConfigClient() as IPolicyConfig);
+
+	try
+        {
+            Marshal.ThrowExceptionForHR(_policyConfigClient.SetDefaultEndpoint(deviceID, ERole.eConsole));
+		    Marshal.ThrowExceptionForHR(_policyConfigClient.SetDefaultEndpoint(deviceID, ERole.eMultimedia));
+		    Marshal.ThrowExceptionForHR(_policyConfigClient.SetDefaultEndpoint(deviceID, ERole.eCommunications));
+		    return 0;
+        }
+        catch
+        {
+            return 1;
+        }
+    }
+
+    public static int SetEndpointVisibility(string deviceID)
+    {
+        IPolicyConfig _policyConfigClient = (new _CPolicyConfigClient() as IPolicyConfig);
+
+	try
+        {
+            Marshal.ThrowExceptionForHR(_policyConfigClient.SetEndpointVisibility(deviceID, false));
+		    return 0;
+        }
+        catch
+        {
+            return 1;
+        }
+    }
+}
+"@
+
+add-type -TypeDefinition $cSharpSourceCode
+
+function Set-DefaultAudioDevice
+{
+    Param
+    (
+        [parameter(Mandatory=$true)]
+        [string[]]
+        $deviceId
+    )
+
+    If ([PolicyConfigClient]::SetDefaultDevice("{0.0.0.00000000}.$deviceId") -eq 0)
+    {
+        Write-Host "SUCCESS: The default audio device has been set."
+    }
+    Else
+    {
+        Write-Host "ERROR: There has been a problem setting the default audio device."
+    }
+}
+
+function Set-EndpointVisibility
+{
+    Param
+    (
+        [parameter(Mandatory=$true)]
+        [string[]]
+        $deviceId
+    )
+
+    If ([PolicyConfigClient]::SetEndpointVisibility("{0.0.0.00000000}.$deviceId") -eq 0)
+    {
+        Write-Host "SUCCESS: The audio device has been disabled."
+    }
+    Else
+    {
+        Write-Host "ERROR: There has been a problem disabling the audio device."
+    }
+}
+# Runs CSharp code to disable audio
+$registryLocation = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\MMDevices\Audio\Render\"
+$audio = Get-ChildItem -Path $registryLocation | ForEach-Object {Get-ItemProperty -Path $_.PsPath | Where-Object {$_.DeviceState -eq 1} | Select-Object PSChildName}
+
+if ($audio.PSChildName -ne $null) {
+    Set-EndpointVisibility $audio.PSChildName
+}
+
 # Sets Proxy Settings
 $registryLocation = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
 
