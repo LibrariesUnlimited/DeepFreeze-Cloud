392eb77c8cb2b5a52822bdcf304476de35c9aa70
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2024-05-20 14:17:58 +0100

    monitor minimise

--- BuildScript.ps1
+++ BuildScript.ps1
@@ -1055,7 +1055,7 @@ switch ($computerPrefix) {
 }
 
 # Disable "Minimize windows when a monitor is disconnected" option to stop iCAM being hidden when someone turns off the monitor
-Set-ItemProperty -Name "MonitorRemovalRecalcBehavior" -Path "HKCU:\Control Panel\Desktop" -Type DWord -Value "0"
+Set-ItemProperty -Name "MonitorRemovalRecalcBehavior" -Path "HKCU:\Control Panel\Desktop" -Type DWord -Value "1"
 
 # Hide unwanted Task Bar icons
 Set-ItemProperty -Name "TaskbarDa" -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Type DWord -Value "0"
