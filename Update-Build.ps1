638cc978107a03f5b60c43f259c3a1d08ca95ab1
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2024-06-18 15:38:45 +0100

    checking for existing values - test

--- Update-Build.ps1
+++ Update-Build.ps1
@@ -4,7 +4,37 @@
 # HOWEVER ... this script should check to see if changes are required to speed processing and restrict errors
 # Eventually all machines will have updates from this script applied so in theory it could be stopped but will be left running to have changes made at next Maintenance Period
 # rather than waiting a week to change the DeepFreeze Cloud Policy
+Start-Transcript -path "C:\Program Files\Libraries Unlimited\Update-Build.Log"
 
+Function Test-RegistryValue {
+    param(
+        [Alias("PSPath")]
+        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [String]$Path
+        ,
+        [Parameter(Position = 1, Mandatory = $true)]
+        [String]$Name
+        ,
+        [Switch]$PassThru
+    ) 
+
+    process {
+        if (Test-Path $Path) {
+            $Key = Get-Item -LiteralPath $Path
+            if ($Key.GetValue($Name, $null) -ne $null) {
+                if ($PassThru) {
+                    Get-ItemProperty $Path $Name
+                } else {
+                    $true
+                }
+            } else {
+                $false
+            }
+        } else {
+            $false
+        }
+    }
+}
 
 # Example ... this didn't work so need to do an HKLM version in this script
 # Disable Copilot Completely (no permissions to create key so probably won't work commenting out for now)
@@ -16,17 +46,44 @@
 
 # Disable Copilot Completely
 $registryLocation = "HKLM:\Software\Policies\Microsoft\Windows\WindowsCopilot"
-if(-not(Test-Path $registryLocation)){
-	New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows" -Name "WindowsCopilot"
+$registryName = "TurnOffWindowsCopilot"
+if(-not(Test-RegistryValue -Path $registryLocation -Name $registryName))
+{
+    write-host "registry value missing copilot"
+    if(-not(Test-Path $registryLocation)){
+	    New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows" -Name "WindowsCopilot"
+    }
+    Set-ItemProperty -Path $registryLocation -Name $registryName -Type DWord -Value "1"
 }
-Set-ItemProperty -Path $registryLocation -Name "TurnOffWindowsCopilot" -Type DWord -Value "1"
+if ((Get-ItemProperty -Path $registryLocation -Name $registryName).TurnOffWindowsCopilot -ne 0) {
+    write-host "registry value wrong copilot"
+    Set-ItemProperty -Path $registryLocation -Name $registryName -Type DWord -Value "1"
+}
 
 # Disable Chrome Full-Tab Promotional Content
 $registryLocation = "HKLM:\Software\Policies\Google\Chrome"
-if(-not(Test-Path "HKLM:\Software\Policies\Google")){
-    New-Item -Path "HKLM:\Software\Policies\" -Name "Google"
+$registryName = "PromotionalTabsEnabled"
+if(-not(Test-RegistryValue -Path $registryLocation -Name $registryName))
+{
+    if(-not(Test-Path "HKLM:\Software\Policies\Google")){
+        New-Item -Path "HKLM:\Software\Policies\" -Name "Google"
+    }
+    if(-not(Test-Path "HKLM:\Software\Policies\Google\Chrome")){
+        New-Item -Path "HKLM:\Software\Policies\Google\" -Name "Chrome"
+    }
+    Set-ItemProperty -Path $registryLocation -Name "PromotionalTabsEnabled" -Type DWord -Value "0" -Force
 }
-if(-not(Test-Path "HKLM:\Software\Policies\Google\Chrome")){
-    New-Item -Path "HKLM:\Software\Policies\Google\" -Name "Chrome"
+if ((Get-ItemProperty -Path $registryLocation -Name $registryName).PromotionalTabsEnabled -ne 0) {
+    Set-ItemProperty -Path $registryLocation -Name "PromotionalTabsEnabled" -Type DWord -Value "0" -Force
 }
-Set-ItemProperty -Path $registryLocation -Name "PromotionalTabsEnabled" -Type DWord -Value "0" -Force
+
+# FAKE VALUE
+$registryLocation = "HKLM:\Software\Policies\Ewen\Chrome"
+$registryName = "PromotionalTabsEnabled"
+if(-not(Test-RegistryValue -Path $registryLocation -Name $registryName))
+{
+ write-host "registry value missing fake"
+}
+
+
+Stop-Transcript
