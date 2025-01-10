5e5c47ef229405f14daad775722e2b7c6a072ae6
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2025-01-10 11:36:58 +0000

    delete softwaredistribution on upstairs exeter PCs

--- Update-Build.ps1
+++ Update-Build.ps1
@@ -5,6 +5,9 @@
 # Eventually all machines will have updates from this script applied so in theory it could be stopped but will be left running to have changes made at next Maintenance Period
 # rather than waiting a week to change the DeepFreeze Cloud Policy
 
+# In addition this script now deletes the SoftwareDistribution folder to clear out corrupted update files each maintenance (for now it will only be on select computers)
+# This section is at the end of the script.
+
 $logFile = "C:\Program Files\Libraries Unlimited\Update-Build.log"
 Start-Transcript $logFile
 Write-Output "Logging to $logFile"
@@ -273,6 +276,39 @@ switch ( $profiles ) {
 }
 
 Write-Output "END OF iCAM CHANGES"
+Write-Output "---------------------"
+
+Write-Output "START OF SOFTWAREDISTRIBUTION DELETION"
+Write-Output "---------------------"
+
+$computerPrefix = $env:COMPUTERNAME.Substring(0,6)
+
+switch ($computerPrefix) {
+    EXEPC3 
+    {
+        try {
+            Write-Verbose 'Clearing SoftwareDistribution\Download folder...'
+            # Create (temporary) empty folder
+            New-Item -ItemType Directory -Path ".\Empty" -ErrorAction SilentlyContinue
+            # Mirror the empty directory to the folder to delete; this will effectively empty the folder.
+            robocopy /MIR ".\Empty" "$env:SystemRoot\SoftwareDistribution\Download" /njh /njs /ndl /nc /ns /np /nfl #>nul 2>&1
+            # Delete the folder now that it's empty
+            Remove-Item "$env:SystemRoot\SoftwareDistribution\Download" -Force
+            # Delete our temporary empty folder
+            Remove-Item ".\Empty" -Force
+        }
+        catch {
+            Write-Error $_
+        }
+        
+        #Then remove all the other folders
+        Write-Verbose 'Clearing SoftwareDistribution folder...'
+        Remove-Item -Path "$env:SystemRoot\SoftwareDistribution\*" -Force -Verbose -Confirm:$false -Recurse -ErrorAction 'SilentlyContinue' -WarningAction 'SilentlyContinue'
+        
+    }
+}
+
+Write-Output "END OF SOFTWAREDISTRIBUTION DELETION"
 Write-Output "---------------------"
 
 Stop-Transcript
