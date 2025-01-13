a303146a5168715aee1608c821eeed26275261f2
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2025-01-13 09:11:07 +0000

    add PEC PCs to update cleanup

--- Update-Build.ps1
+++ Update-Build.ps1
@@ -306,6 +306,28 @@ switch ($computerPrefix) {
         Remove-Item -Path "$env:SystemRoot\SoftwareDistribution\*" -Force -Verbose -Confirm:$false -Recurse -ErrorAction 'SilentlyContinue' -WarningAction 'SilentlyContinue'
         
     }
+    NEWPC0 
+    {
+        try {
+            Write-Verbose 'Clearing SoftwareDistribution\Download folder...'
+            # Create (temporary) empty folder
+            New-Item -ItemType Directory -Path ".\Empty" -ErrorAction SilentlyContinue
+            # Mirror the empty directory to the folder to delete; this will effectively empty the folder.
+            robocopy /MIR ".\Empty" "$env:SystemRoot\SoftwareDistribution\Download" /njh /njs /ndl /nc /ns /np /nfl #>nul 2>&1
+            # Delete the folder now that it's empty
+            Remove-Item "$env:SystemRoot\SoftwareDistribution\Download" -Force
+            # Delete our temporary empty folder
+            Remove-Item ".\Empty" -Force
+        }
+        catch {
+            Write-Error $_
+        }
+        
+        #Then remove all the other folders
+        Write-Verbose 'Clearing SoftwareDistribution folder...'
+        Remove-Item -Path "$env:SystemRoot\SoftwareDistribution\*" -Force -Verbose -Confirm:$false -Recurse -ErrorAction 'SilentlyContinue' -WarningAction 'SilentlyContinue'
+        
+    }
 }
 
 Write-Output "END OF SOFTWAREDISTRIBUTION DELETION"
