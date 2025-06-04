f459f6b16b996507feffdcb4b137ba9e1899aa4e
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2025-06-04 14:45:03 +0100

    eventlog all code

--- TEST-EventViewer.ps1
+++ TEST-EventViewer.ps1
@@ -9,17 +9,17 @@ Write-Output "###### START $(Get-Date -Format "dd-MM-yyyy HH:mm:ss") ##### "
 Get-ChildItem "HKLM:\SYSTEM\CurrentControlSet\Services\EventLog"
 
 # Expected Locations
-#$application = "C:\Windows\EventLogs\Application.evtx"
-#$hardwareEvents = "C:\Windows\EventLogs\HardwareEvents.evtx"
-#$security = "C:\Windows\EventLogs\Security.evtx"
-#$system = "C:\Windows\EventLogs\System.evtx"
+$application = "C:\Windows\EventLogs\Application.evtx"
+$hardwareEvents = "C:\Windows\EventLogs\HardwareEvents.evtx"
+$security = "C:\Windows\EventLogs\Security.evtx"
+$system = "C:\Windows\EventLogs\System.evtx"
 
 # PhaseOneTest Test Locations
 # Expected Locations
-$application = "C:\windows\system32\winevt\Logs\Application.evtx"
-$hardwareEvents = "C:\windows\system32\winevt\Logs\HardwareEvents.evtx"
-$security = "C:\windows\system32\winevt\Logs\Security.evtx"
-$system = "C:\windows\system32\winevt\Logs\System.evtx"
+#$application = "C:\windows\system32\winevt\Logs\Application.evtx"
+#$hardwareEvents = "C:\windows\system32\winevt\Logs\HardwareEvents.evtx"
+#$security = "C:\windows\system32\winevt\Logs\Security.evtx"
+#$system = "C:\windows\system32\winevt\Logs\System.evtx"
 
 # Application check and change
 $registryLocation = "HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\Application"
@@ -27,6 +27,33 @@ if ((Get-ItemProperty -Path $registryLocation -Name "File")."File" -ne $applicat
     Set-ItemProperty -Path $registryLocation -Name "File" -Type ExpandString -Value $application -Force
     Set-ItemProperty -Path $registryLocation -Name "Flags" -Type DWord -Value 1 -Force
     Write-Output "Application Eventlog Incorrect"
+    Write-Output "---------------------"
+}
+
+# Hardware Events check and change
+$registryLocation = "HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\HardwareEvents"
+if ((Get-ItemProperty -Path $registryLocation -Name "File")."File" -ne $hardwareEvents) {
+    Set-ItemProperty -Path $registryLocation -Name "File" -Type ExpandString -Value $hardwareEvents -Force
+    Set-ItemProperty -Path $registryLocation -Name "Flags" -Type DWord -Value 1 -Force
+    Write-Output "Hardware Events Eventlog Incorrect"
+    Write-Output "---------------------"
+}
+
+# Security check and change
+$registryLocation = "HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\Security"
+if ((Get-ItemProperty -Path $registryLocation -Name "File")."File" -ne $security) {
+    Set-ItemProperty -Path $registryLocation -Name "File" -Type ExpandString -Value $security -Force
+    Set-ItemProperty -Path $registryLocation -Name "Flags" -Type DWord -Value 1 -Force
+    Write-Output "Security Eventlog Incorrect"
+    Write-Output "---------------------"
+}
+
+# System check and change
+$registryLocation = "HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\System"
+if ((Get-ItemProperty -Path $registryLocation -Name "File")."File" -ne $system) {
+    Set-ItemProperty -Path $registryLocation -Name "File" -Type ExpandString -Value $system -Force
+    Set-ItemProperty -Path $registryLocation -Name "Flags" -Type DWord -Value 1 -Force
+    Write-Output "System Eventlog Incorrect"
     Write-Output "---------------------"
 }
 
