5e94492473e0281d1a1bb7e7e6c30a54af485896
Ewen Robinson <ewen.robinson@librariesunlimited.org.uk>
2026-04-10 11:20:08 +0100

    add nic advancedproperty control

--- Update-Build.ps1
+++ Update-Build.ps1
@@ -502,60 +502,12 @@ if(-not(Test-Path -Path "C:\Program Files\Libraries Unlimited\Update-UpdateFix.p
 Write-Output "END OF UPDATE FIX"
 Write-Output "---------------------"
 
-#Write-Output "START OF SOFTWAREDISTRIBUTION DELETION"
-#Write-Output "---------------------"
+Write-Output "START OF NIC SETTINGS"
+Write-Output "---------------------"
 
-#$computerPrefix = $env:COMPUTERNAME.Substring(0,6)
+Set-NetAdapterAdvancedProperty -Name "Ethernet" -DisplayName "Energy Efficient Ethernet" -DisplayValue Off
 
-#switch ($computerPrefix) {
-#    EXEPC3 
-#    {
-#        try {
-#            Write-Verbose 'Clearing SoftwareDistribution\Download folder...'
-#            # Create (temporary) empty folder
-#            New-Item -ItemType Directory -Path ".\Empty" -ErrorAction SilentlyContinue
-#            # Mirror the empty directory to the folder to delete; this will effectively empty the folder.
-#            robocopy /MIR ".\Empty" "$env:SystemRoot\SoftwareDistribution\Download" /njh /njs /ndl /nc /ns /np /nfl #>nul 2>&1
-#            # Delete the folder now that it's empty
-#            Remove-Item "$env:SystemRoot\SoftwareDistribution\Download" -Force
-#            # Delete our temporary empty folder
-#            Remove-Item ".\Empty" -Force
-#        }
-#        catch {
-#            Write-Error $_
-#        }
-        
-#        #Then remove all the other folders
-#        Write-Verbose 'Clearing SoftwareDistribution folder...'
-#        Remove-Item -Path "$env:SystemRoot\SoftwareDistribution\*" -Force -Verbose -Confirm:$false -Recurse -ErrorAction 'SilentlyContinue' -WarningAction 'SilentlyContinue'
-        
-#    }
-#    NEWPC0 
-#    {
-#        try {
-#            Write-Verbose 'Clearing SoftwareDistribution\Download folder...'
-#            # Create (temporary) empty folder
-#            New-Item -ItemType Directory -Path ".\Empty" -ErrorAction SilentlyContinue
-#            # Mirror the empty directory to the folder to delete; this will effectively empty the folder.
-#            robocopy /MIR ".\Empty" "$env:SystemRoot\SoftwareDistribution\Download" /njh /njs /ndl /nc /ns /np /nfl #>nul 2>&1
-#            # Delete the folder now that it's empty
-#            Remove-Item "$env:SystemRoot\SoftwareDistribution\Download" -Force
-#            # Delete our temporary empty folder
-#            Remove-Item ".\Empty" -Force
-#        }
-#        catch {
-#            Write-Error $_
-#        }
-        
-#        #Then remove all the other folders
-#        Write-Verbose 'Clearing SoftwareDistribution folder...'
-#        Remove-Item -Path "$env:SystemRoot\SoftwareDistribution\*" -Force -Verbose -Confirm:$false -Recurse -ErrorAction 'SilentlyContinue' -WarningAction 'SilentlyContinue'
-        
-#    }
-#}
-
-
-#Write-Output "END OF SOFTWAREDISTRIBUTION DELETION"
-#Write-Output "---------------------"
+Write-Output "END OF NIC SETTINGS"
+Write-Output "---------------------"
 
 Stop-Transcript
